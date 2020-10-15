2e1b0c10b96904ee0d6fd49d9eb2f56870a34dca
Carly McKelvy <carlymckelvy@hotmail.com>
2020-10-14 18:31:05 -0700

    updating files

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,18 +1,18 @@
-var svgWidth = 960;
+var svgWidth = 690;
 var svgHeight = 500;
 
 var margin = {
   top: 20,
   right: 40,
   bottom: 60,
-  left: 100
+  left: 40
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".scatter")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -21,7 +21,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data.csv").then(healthData => {
+d3.csv("assets/data/data.csv").then(healthData => {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -34,11 +34,13 @@ d3.csv("assets/data.csv").then(healthData => {
     // ==============================
     var xLinearScale = d3.scaleLinear()
       .domain([20, d3.max(healthData, d => d.obesity)])
-      .range([0, width]);
+      .range([0, width])
+      .nice();
 
     var yLinearScale = d3.scaleLinear()
       .domain([0, d3.max(healthData, d => d.poverty)])
-      .range([height, 0]);
+      .range([height, 0])
+      .nice();
 
     // Step 3: Create axis functions
     // ==============================
@@ -62,23 +64,28 @@ d3.csv("assets/data.csv").then(healthData => {
     .attr("cx", d => xLinearScale(d.obesity))
     .attr("cy", d => yLinearScale(d.poverty))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("class", "stateCircle")
     .attr("opacity", 0.5)
-    .attr("stroke", "black")
     .attr("stroke-width", 1);
 
-    // circlesGroup.append(null)
-    //     .data(healthData)
-    //     .text(function (d) {return " ( " + d.cx + ", " + d.cy +" )"; })
-    //     .attr("font-family", "sans-serif")
-    //     .attr("font-size", "20px")
-    //     .attr("fill", "red")
-    //     .attr("text-anchor", "middle");
-
+    //Add text to circles
+    circlesGroup.append("g")
+      .selectAll("text")
+      .data(healthData)
+      .join("text")
+      .append("text")
+      .attr("x", d => xLinearScale(d.obesity))
+      .attr("y", d => yLinearScale(d.poverty))
+      .attr("class", "stateText")
+      .style("font-weight", "bold")
+      .attr("font-size", "10px")
+      .attr("font-color", "blue")
+      .text(d => d.abbr);
+      
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
-      .attr("class", "tooltip")
+      .attr("class", "d3-tip")
       .offset([80, -60])
       .html(d => `${d.state}<br>Obesity: ${d.obesity}<br>Poverty: ${d.poverty}`);
 
@@ -102,11 +109,12 @@ d3.csv("assets/data.csv").then(healthData => {
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
-      .attr("class", "axisText")
+      .attr("class", "aText")
       .text("Poverty");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
+      .attr("class", "aText")
       .text("Obesity");
+
   }).catch(error => console.log(error));
